3996a1efcbb5a84ddd5cff7f0f1dfbe256f53e74
cric333 <cricketzone333@gmail,com>
2024-05-29 00:48:11 +0530

    Commit

--- index.js
+++ index.js
@@ -42,7 +42,8 @@ connectToDatabase();
       // const parsedData = JSON.parse(storedData);
       const db = client.db('data');
       const collection = db.collection('main');
-      const parsedData = await collection.findOne({});
+      const objectId = new ObjectId('6656111a7a5d01ddd0a81ec8');
+      const parsedData = await collection.findOne({ _id: objectId });
       const userIds = {
         '/api/data1': parsedData.id1,
         '/api/data2': parsedData.id2,
@@ -85,7 +86,8 @@ connectToDatabase();
     // const parsedData = JSON.parse(storedData);
     const db = client.db('data');
     const collection = db.collection('main');
-    const parsedData = await collection.findOne({});
+    const objectId = new ObjectId('6656111a7a5d01ddd0a81ec8');
+    const parsedData = await collection.findOne({ _id: objectId });
 
     const profileIds = {
       '/api/profile1': parsedData.id1,
@@ -93,6 +95,185 @@ connectToDatabase();
       '/api/profile3': parsedData.id3,
       '/api/profile4': parsedData.id4,
       '/api/profile5': parsedData.id5
+    };
+    try {
+      const profile = profileIds[req.path];
+      if (!profile) {
+        throw new Error('User ID not found for this route');
+      }
+      const headers = {
+        'Accept-Encoding': 'gzip',
+        'Accept-Language': 'en',
+        'appId': 'in.probo.pro',
+        'Authorization': 'Bearer 6gbXLGYJE706+r3w0wbznHxLOh4O0IcerWuX93LMunQ=',
+        'Cache-Control': 'public, max-age=60',
+        'Connection': 'Keep-Alive',
+        'Content-Type': 'application/json',
+        'Host': 'prod.api.probo.in',
+        'If-None-Match': 'W/"ab3-sEymgQEpEGCaSnklL7Rmvl7Oxmc"',
+        'session_id': 'fb334833-442b-4db7-948e-845cb6458cf8',
+        'User-Agent': 'okhttp/4.10.0',
+        'x-device-id': '32db1ea301e4eff8',
+        'x-device-os': 'ANDROID',
+        'x-os-version': '31',
+        'x-version-code': '301',
+        'x-version-name': '5.81.4'
+      };
+  
+      // Make request to the API
+      const response = await axios.get(`https://prod.api.probo.in/api/v2/user/${profile}/profile`, { headers });
+      res.json(response.data);
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+
+  app.get([ '/api/cricket/data1','/api/cricket/data2', '/api/cricket/data3', '/api/cricket/data4', '/api/cricket/data5'], async (req, res) => {
+    try {
+      const db = client.db('data');
+      const collection = db.collection('main');
+      const objectId = new ObjectId('665611607a5d01ddd0a81ecd');
+      const parsedData = await collection.findOne({ _id: objectId });
+      const userIds = {
+        '/api/cricket/data1': parsedData.id1,
+        '/api/cricket/data2': parsedData.id2,
+        '/api/cricket/data3': parsedData.id3,
+        '/api/cricket/data4': parsedData.id4,
+        '/api/cricket/data5': parsedData.id5
+      };
+      const userId = userIds[req.path];
+      if (!userId) {
+        throw new Error('User ID not found for this route');
+      }
+      const headers = {
+        'Accept-Encoding': 'gzip',
+        'Accept-Language': 'en',
+        'appId': 'in.probo.pro',
+        'Authorization': 'Bearer 6gbXLGYJE706+r3w0wbznHxLOh4O0IcerWuX93LMunQ=',
+        'Connection': 'Keep-Alive',
+        'Content-Type': 'application/json',
+        'Host': 'prod.api.probo.in',
+        'If-None-Match': 'W/"2f-wzv4xhcQLCJjwXnFBenza8nQjdk"',
+        'session_id': 'fb334833-442b-4db7-948e-845cb6458cf8',
+        'User-Agent': 'okhttp/4.10.0',
+        'x-device-id': '32db1ea301e4eff8',
+        'x-device-os': 'ANDROID',
+        'x-os-version': '31',
+        'x-version-code': '301',
+        'x-version-name': '5.81.4'
+      };
+
+      // Make request to the API
+      const response = await axios.get(`https://prod.api.probo.in/api/v1/user/${userId}/tradedEvents?page=1&live_event=1`, { headers });
+      res.json(response.data);
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+
+  app.get([ '/api/cricket/profile1','/api/cricket/profile2', '/api/cricket/profile3', '/api/cricket/profile4', '/api/cricket/profile5'], async (req, res) => {
+    // const storedData = fs.readFileSync(FILE_PATH, 'utf8');
+    // const parsedData = JSON.parse(storedData);
+    const db = client.db('data');
+    const collection = db.collection('main');
+    const objectId = new ObjectId('665611607a5d01ddd0a81ecd');
+    const parsedData = await collection.findOne({ _id: objectId });
+  
+    const profileIds = {
+      '/api/cricket/profile1': parsedData.id1,
+      '/api/cricket/profile2': parsedData.id2,
+      '/api/cricket/profile3': parsedData.id3,
+      '/api/cricket/profile4': parsedData.id4,
+      '/api/cricket/profile5': parsedData.id5
+    };
+    try {
+      const profile = profileIds[req.path];
+      if (!profile) {
+        throw new Error('User ID not found for this route');
+      }
+      const headers = {
+        'Accept-Encoding': 'gzip',
+        'Accept-Language': 'en',
+        'appId': 'in.probo.pro',
+        'Authorization': 'Bearer 6gbXLGYJE706+r3w0wbznHxLOh4O0IcerWuX93LMunQ=',
+        'Cache-Control': 'public, max-age=60',
+        'Connection': 'Keep-Alive',
+        'Content-Type': 'application/json',
+        'Host': 'prod.api.probo.in',
+        'If-None-Match': 'W/"ab3-sEymgQEpEGCaSnklL7Rmvl7Oxmc"',
+        'session_id': 'fb334833-442b-4db7-948e-845cb6458cf8',
+        'User-Agent': 'okhttp/4.10.0',
+        'x-device-id': '32db1ea301e4eff8',
+        'x-device-os': 'ANDROID',
+        'x-os-version': '31',
+        'x-version-code': '301',
+        'x-version-name': '5.81.4'
+      };
+  
+      // Make request to the API
+      const response = await axios.get(`https://prod.api.probo.in/api/v2/user/${profile}/profile`, { headers });
+      res.json(response.data);
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+
+  app.get([ '/api/youtube/data1','/api/youtube/data2', '/api/youtube/data3', '/api/youtube/data4', '/api/youtube/data5'], async (req, res) => {
+    try {
+      const db = client.db('data');
+      const collection = db.collection('main');
+      const objectId = new ObjectId('6656116a7a5d01ddd0a81ece');
+      const parsedData = await collection.findOne({ _id: objectId });
+      const userIds = {
+        '/api/youtube/data1': parsedData.id1,
+        '/api/youtube/data2': parsedData.id2,
+        '/api/youtube/data3': parsedData.id3,
+        '/api/youtube/data4': parsedData.id4,
+        '/api/youtube/data5': parsedData.id5
+      };
+      const userId = userIds[req.path];
+      if (!userId) {
+        throw new Error('User ID not found for this route');
+      }
+      const headers = {
+        'Accept-Encoding': 'gzip',
+        'Accept-Language': 'en',
+        'appId': 'in.probo.pro',
+        'Authorization': 'Bearer 6gbXLGYJE706+r3w0wbznHxLOh4O0IcerWuX93LMunQ=',
+        'Connection': 'Keep-Alive',
+        'Content-Type': 'application/json',
+        'Host': 'prod.api.probo.in',
+        'If-None-Match': 'W/"2f-wzv4xhcQLCJjwXnFBenza8nQjdk"',
+        'session_id': 'fb334833-442b-4db7-948e-845cb6458cf8',
+        'User-Agent': 'okhttp/4.10.0',
+        'x-device-id': '32db1ea301e4eff8',
+        'x-device-os': 'ANDROID',
+        'x-os-version': '31',
+        'x-version-code': '301',
+        'x-version-name': '5.81.4'
+      };
+
+      // Make request to the API
+      const response = await axios.get(`https://prod.api.probo.in/api/v1/user/${userId}/tradedEvents?page=1&live_event=1`, { headers });
+      res.json(response.data);
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+
+  app.get([ '/api/youtube/profile1','/api/youtube/profile2', '/api/youtube/profile3', '/api/youtube/profile4', '/api/youtube/profile5'], async (req, res) => {
+    // const storedData = fs.readFileSync(FILE_PATH, 'utf8');
+    // const parsedData = JSON.parse(storedData);
+    const db = client.db('data');
+    const collection = db.collection('main');
+    const objectId = new ObjectId('6656116a7a5d01ddd0a81ece');
+    const parsedData = await collection.findOne({ _id: objectId });
+    const profileIds = {
+      '/api/youtube/profile1': parsedData.id1,
+      '/api/youtube/profile2': parsedData.id2,
+      '/api/youtube/profile3': parsedData.id3,
+      '/api/youtube/profile4': parsedData.id4,
+      '/api/youtube/profile5': parsedData.id5
     };
     try {
       const profile = profileIds[req.path];
